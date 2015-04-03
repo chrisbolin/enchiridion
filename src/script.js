071a4a8455c646ec9e95f33f4b6dd9bae4576fca
Chris Bolin <bolin.chris@gmail.com>
2015-04-03 00:50:42 -0400

    adding forward and back navigation

--- src/script.js
+++ src/script.js
@@ -1,6 +1,11 @@
 var text = '';
+var state = {};
 
 var init = function(){
+  downloadText();
+};
+
+var downloadText = function(){
   var request = new XMLHttpRequest();
   request.open('GET', '/enchiridion.txt');
   request.onreadystatechange = function() {
@@ -11,15 +16,37 @@ var init = function(){
 };
 
 var renderPage = function(){
-  var page = document.getElementById('page');
+  var page = document.getElementById('page-text');
   page.innerHTML = prepareText();
 };
 
 var prepareText = function(){
-  var pageText = text;
+  var hashNumber = parseInt(location.hash.substr(1));
+  state.start =  (hashNumber > 0)? hashNumber : 0;
+  state.offset = 600;
+  
+  var pageText = text.substring(state.start, state.start + state.offset);
   pageText = '<p>' + pageText + '</p>'; // wrap in <p>
   pageText = pageText.replace(/\n\n/g,'</p><p>'); // double return -> new <p> 
+  
   return pageText;
 };
+
+var flipPage = function(back){
+  var direction = back? -1 : 1;
+  location.hash = '#' + (state.start + direction * state.offset);
+};
+
+document.getElementById('forward-button').addEventListener('click', function() {
+  flipPage();
+}, false);
+
+document.getElementById('back-button').addEventListener('click', function() {
+  flipPage(true);
+}, false);
+
+window.addEventListener("hashchange", function(){
+  renderPage();
+}, false);
 
 init();
